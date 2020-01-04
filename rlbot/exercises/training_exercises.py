1a1bc57ed49435050ee5ff7384a4e3c378846d2c
Chainso <chanzobryan@gmail.com>
2020-01-04 00:04:57 -0500

    Renaming and reorganizing

--- rlbot/exercises/training_exercises.py
+++ rlbot/exercises/training_exercises.py
@@ -6,5 +6,5 @@ from rlbot.matchconfig.match_config import PlayerConfig, Team
 def make_default_playlist():
     for exercise in mdp():
         exercise.match_config.player_configs = [
-            PlayerConfig.bot_config(Path("../src/bot.cfg"), Team.BLUE)
+            PlayerConfig.bot_config(Path("src/bot.cfg"), Team.BLUE)
         ]
