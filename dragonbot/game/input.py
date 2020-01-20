e5ca64981901802885130a4ec1d82d74297ee5f3
Chainso <chanzobryan@gmail.com>
2020-01-20 11:59:03 -0500

    Finished transform packet for the formatter

--- dragonbot/game/input.py
+++ dragonbot/game/input.py
@@ -1,6 +1,15 @@
 import torch
 
 class InputFormatter():
+    FLOOR = 0
+    CEILING = 2044
+    MIDDLE_HEIGHT = (CEILING - FLOOR) / 2
+    CENTER_FIELD = (0, 0, 0)
+    BACK_WALL_LENGTH = 5120
+    SIDE_WALL_LENGTH = 4096
+    GOAL_HEIGHT = 642.775
+    GOAL_CENTER_TO_POST = 892.755
+
     """
     A class to format game input to feed into the model.
     """
@@ -9,18 +18,67 @@ class InputFormatter():
         self.index = index
         self.device = torch.device(device)
 
-    def _get_car_info(self, car):
+    def get_obj_info(self, car):
         """
         Gets the relevant information for a game car.
         """
         physics = car.physics
 
+        loc = physics.location
+        rot = physics.rotation
+        vel = physics.velocity
+        ang_v = physics.angular_velocity
+
+        # Normalize all values, dividing by 1000 for now until max/min
+        # velocities are attained
+        #loc_x = loc.x / SIDE_WALL_LENGTH
+        #loc_y = loc.y / BACK_WALL_LENGTH
+        #loc_z = (loc.z - MIDDLE_HEIGHT) / MIDDLE_HEIGHT
+
+
+        loc_vel = torch.FloatTensor([
+            loc.x, loc.y, loc.z,
+            vel.x, vel.y, vel.z,
+            ang_v.x, ang_v.y, ang_v.z
+        ]).to(self.device)
+        loc_vel = loc_vel / 1000
+
+        rot_info = torch.FloatTensor([
+            rot.pitch, rot.yaw, rot.roll,
+        ]).to(self.device)
+
+        car_info = torch.cat(loc_vel, rot_info, dim=0)
+
+        return car_info
+
     def transform_packet(self, packet):
         """
         Transforms the packet into a state to feed into the model.
         """
-        cars_info = []
-        return torch.FloatTensor([state]).to(device)
+        # It's your car first, then your team, then the enemy team
+        my_team_info = []
+        enemy_team_info = []
+
+        for i in range(len(packet.game_cars)):
+            car = packet.game_cars[i]
+            car_info = self.get_obj_info(car)
+
+            if i == self.index:
+                boost = torch.FloatTensor([packet.num_boost / 100])
+                my_car_info = torch.cat(car_info, boost)
+                my_team_info = [my_car_info] + [my_team_info]
+            elif car.team == self.team:
+                my_team_info.append(car_info)
+            else:
+                enemy_team_info.append(car_info)
+
+        car_infos = torch.cat(*(my_team_info + enemy_team_info))
+
+        ball_info = self.get_obj_info(packet.game_ball)
+
+        all_info = torch.cat(car_infos, ball_info)
+
+        return all_info
 
     @staticmethod
     def state_space():
